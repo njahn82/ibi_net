3ca1417aabb21c88652c6486c111355532314c31
naja <najko.jahn@gmail.com>
2021-10-22 22:02:00 +0200

    make it binder ready

---
+++ README.md
@@ -0,0 +1,3 @@
+### Praxismodul Bibliometrie M26 IBI HU
+
+https://mybinder.org/v2/gh/njahn82/ibi_net/HEAD
---
+++ install.R
@@ -0,0 +1,4 @@
+install.packages(c(
+  "remotes", "tidyverse", "sna", "network", "ggnet"
+))
+remotes::install_github("ropensci/rcrossref")
